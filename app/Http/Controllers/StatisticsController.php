481219ac0b30c363f46b38fd573d032e80411040
Yan <rbeinyan@gmail.com>
2024-03-04 18:13:11 -0300

    StatisticsController hotfix

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -6,7 +6,7 @@ use App\Services\StatisticsCalculator;
 
 class StatisticsController extends Controller
 {
-    public function statistics(StatisticsCalculator $calculator) {
+    public function index(StatisticsCalculator $calculator) {
         return response()->json([
             'popularHour' => $calculator->getMostPopularHour(),
             'topQueries' => $calculator->getTopQueries(),
